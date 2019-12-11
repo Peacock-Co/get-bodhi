d87952cf298d6631a2e64dcb8287d289ea4153b6
Peacock-Co <pavaoleonardo@hotmail.com>
2019-12-11 15:07:07 +0100

    Add background

--- js/game.js
+++ js/game.js
@@ -1,43 +1,17 @@
-document.addEventListener("keydown", function moveRight(evento) {
-  if (evento.keyCode === 39) {
-    console.log("right");
+let game;
+class GameCanvas {
+  constructor() {
+    this.canvasWidth = 700;
+    this.canvasHeight = 700;
+    this.ctx = document.getElementById("canvas").getContext("2d");
+
+    this.background = new Background(
+      this.ctx,
+      this.canvasWidth,
+      this.canvasHeight
+    );
   }
-});
-
-document.addEventListener("keydown", function moveLeft(evento) {
-  if (evento.keyCode === 37) {
-    console.log("left");
+  draw() {
+    this.background.draw();
   }
-});
-
-let imgJonny, imgNube, imgBirds;
-
-function loadImg() {
-  imgJonny = new Image();
-}
-
-let width = 700;
-let height = 700;
-
-let canvas, ctx;
-
-function inicialize() {
-  canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
-  loadImg();
-}
-
-function clear() {
-  canvas.width = height;
-  canvas.height = height;
-}
-
-// Rendering
-let FPS = 10;
-setInterval(() => {
-  main();
-}, 1000 / 10);
-
-function main() {
-  clear();
 }
