e70e80d819920ed85f252b0d7fe80a05f1a80fd6
Peacock-Co <pavaoleonardo@hotmail.com>
2019-12-16 12:43:45 +0100

    Added colision and clear the bird

--- js/game.js
+++ js/game.js
@@ -20,6 +20,7 @@ class GameCanvas {
     this.score = 0;
     this.time = 3;
     this.birds = [];
+    this.counter = 0;
     this.intervalId;
   }
 
@@ -27,19 +28,23 @@ class GameCanvas {
     this.interval = setInterval(() => {
       this.framesCounter++;
 
-      this.clear();
       this.drawAll();
       this.moveAll();
       this.moveBirds();
+
+      this.stop();
+      //if (this.checkCollision()) this.gameOver();
 
       if (this.framesCounter % 60 == 0) {
         //velocidad de la aparición de pajaros
         this.time--;
         this.generateBird();
       }
-
       this.checkCollision(this.birds, "bird");
+      this.clearBird();
+
       //this.clearBirds(); //lo nombras para que te haga caso limpiar
+      //if (this.isCollision()) this.gameOver();
 
       this.framesCounter =
         this.framesCounter > 1000
@@ -52,30 +57,38 @@ class GameCanvas {
     if (this.isCollision(arrayColision)) {
       //añadir todo lo que quieras que pase cuando haga colision
       if (value == "bird") {
-        this.time -= 1; //resta +1 la vida
-        this.johnny.img.framesCounter = 1;
+        this.time -= 1; //resta -1 la vida
+        this.johnny.framesCounter = 1;
+        this.stop();
       }
     }
     setTimeout(
       function() {
         //reciba la segunda imagen y le aplica el tiempo en cambiar
-        this.johnny.img.framesCounter = 0;
+        this.johnny.framesCounter = 0;
       }.bind(this),
       500
     );
   }
 
-  // clearBirds() {
-  //   //limpia el canvas
-  //   this.birds = this.birds.filter(
-  //     function(birds) {
-  //       return birds.y < this.canvas.height && !birds.hit;
-  //     }.bind(this)
-  //   );
-  // }
-
-  clear() {
-    this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+  isCollision(arrayCollision) {
+    //pinta las colisiones
+    // (p.x + p.w > o.x && o.x + o.w > p.x && p.y + p.h > o.y && o.y + o.h > p.y )
+    return arrayCollision.some(
+      function(birds) {
+        var collision =
+          this.johnny.x + this.johnny.width >= birds.x &&
+          this.johnny.x <= birds.x + birds.width &&
+          this.johnny.y + this.johnny.height >= birds.y &&
+          birds.y + birds.height >= this.johnny.y;
+        if (collision) {
+          birds.hit = true;
+          this.score++;
+          console.log(this.score);
+        }
+        return collision;
+      }.bind(this)
+    );
   }
 
   drawAll() {
@@ -105,27 +118,25 @@ class GameCanvas {
     this.birds.push(new Birds(this)); //dibuja el array vacio this.birds[]
   }
 
-  isCollision(arrayCollision) {
-    //pinta las colisiones
-    // (p.x + p.w > o.x && o.x + o.w > p.x && p.y + p.h > o.y && o.y + o.h > p.y )
-    return arrayCollision.some(
+  clearBird() {
+    this.birds = this.birds.filter(
       function(birds) {
-        var collision =
-          this.johnny.x + this.johnny.width >= birds.x &&
-          this.johnny.x <= birds.x + birds.width &&
-          this.johnny.y + this.johnny.height >= birds.y &&
-          birds.y + birds.height >= this.johnny.y;
-        if (collision) {
-          birds.hit = true;
-        }
-        return collision;
+        return birds.y < this.canvasHeight, this.canvasWidth && !birds.hit;
       }.bind(this)
     );
+  }
+
+  clear() {
+    this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
   }
 
   stop() {
     if (this.time <= 0) {
       clearInterval(this.intervalId);
     }
+  }
+
+  gameOver() {
+    clearInterval(this.interval);
   }
 }
